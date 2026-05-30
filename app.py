8dcbaabc80894abc8fd5cf338d8c47c9b4a00141
ajayecho111-commits <ajayecho111@gmail.com>
2026-05-31 00:42:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 import yfinance as yf
 
 st.title("Mera Trading Dashboard")
-st.write("Abhi hum data load karna seekh rahe hain...")
+
+# Stock ka data mangwana
+ticker = "RELIANCE.NS"
+data = yf.download(ticker, period="1d")
+
+st.write("Data aa gaya:")
+st.write(data)
